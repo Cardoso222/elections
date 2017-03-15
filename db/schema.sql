dc010f7329fdbf99884cc5d2380481c3f82c041b
cardoso222 <paulocardoso@infojr.com.br>
2017-03-15 20:28:52 -0300

    add new schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,6 @@
-CREATE DATABASE  IF NOT EXISTS `mydb` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `mydb`;
 -- MySQL dump 10.13  Distrib 5.5.54, for debian-linux-gnu (x86_64)
 --
--- Host: 127.0.0.1    Database: mydb
+-- Host: localhost    Database: mydb
 -- ------------------------------------------------------
 -- Server version	5.5.54-0ubuntu0.14.04.1
 
@@ -32,8 +30,18 @@ CREATE TABLE `candidates` (
   `pic_name` varchar(45) DEFAULT NULL,
   `electionId` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `candidates`
+--
+
+LOCK TABLES `candidates` WRITE;
+/*!40000 ALTER TABLE `candidates` DISABLE KEYS */;
+INSERT INTO `candidates` VALUES (1,'obama',40,'Democratas','obama.jpg',1),(2,'Dilma',50,'PT','dilma.jpg',1),(3,'Temer',42,'PMDB','temer.jpg',1),(4,'Paulo',22,'CD','b4f67c2cbab3ab3f7891c4c9098a2126',1),(5,'Paulo',22,'CA','41fc6854e03858bbf0a2e369e948df05',2),(6,'Gedel',40,'AA','0477a875c23e88d4273bddab8ebc6869',3),(7,'Antonio',40,'JJ','f828098551a71fcae399bbb7422cd565',4);
+/*!40000 ALTER TABLE `candidates` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `electionStatus`
@@ -50,6 +58,16 @@ CREATE TABLE `electionStatus` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `electionStatus`
+--
+
+LOCK TABLES `electionStatus` WRITE;
+/*!40000 ALTER TABLE `electionStatus` DISABLE KEYS */;
+INSERT INTO `electionStatus` VALUES (0,'Finalizada'),(1,'Em andamento');
+/*!40000 ALTER TABLE `electionStatus` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `elections`
 --
 
@@ -64,8 +82,18 @@ CREATE TABLE `elections` (
   `endDate` varchar(45) DEFAULT NULL,
   `url_friendly` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `elections`
+--
+
+LOCK TABLES `elections` WRITE;
+/*!40000 ALTER TABLE `elections` DISABLE KEYS */;
+INSERT INTO `elections` VALUES (2,'Presidente da UFBA',1,'09/03/2017','16/03/2017','opdrw'),(3,'Vice-Presidente',1,'10/03/2017','25/03/2017','pokbl'),(4,'Senador',1,'10/03/2017','11/03/2017','vaecr');
+/*!40000 ALTER TABLE `elections` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
@@ -81,14 +109,24 @@ CREATE TABLE `users` (
   `registration` int(45) DEFAULT NULL,
   `cpf` varchar(11) NOT NULL,
   `course` varchar(45) DEFAULT NULL,
-  `password` varchar(45) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
   `type` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
   UNIQUE KEY `cpf_UNIQUE` (`cpf`),
   UNIQUE KEY `email_UNIQUE` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Usuario','user@ufba.br',213103033,'1646332985','BSI','f39b5519a03f920638de6e1c0e27476e6687bdbf8c5d648907fb81875d4e022da1f06e744156de1975acc57095f84cc31631b1feedab5620193f916b6577a826',2),(2,'Admin','admin@ufba.br',NULL,'16465632985',NULL,'f39b5519a03f920638de6e1c0e27476e6687bdbf8c5d648907fb81875d4e022da1f06e744156de1975acc57095f84cc31631b1feedab5620193f916b6577a826',1),(4,'Paulo Henrique','ph@ufba.br',NULL,'8591811551','BSI','f39b5519a03f920638de6e1c0e27476e6687bdbf8c5d648907fb81875d4e022da1f06e744156de1975acc57095f84cc31631b1feedab5620193f916b6577a826',2);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `usersHistory`
@@ -103,8 +141,18 @@ CREATE TABLE `usersHistory` (
   `electionId` int(11) DEFAULT NULL,
   `createAt` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usersHistory`
+--
+
+LOCK TABLES `usersHistory` WRITE;
+/*!40000 ALTER TABLE `usersHistory` DISABLE KEYS */;
+INSERT INTO `usersHistory` VALUES (2,1,3,NULL),(3,1,4,NULL),(4,4,2,NULL),(5,4,4,NULL),(6,4,3,NULL),(7,4,3,NULL),(8,4,3,NULL),(9,1,2,'2017-03-09 19:21:56');
+/*!40000 ALTER TABLE `usersHistory` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `votes`
@@ -119,8 +167,18 @@ CREATE TABLE `votes` (
   `candidateId` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `votes`
+--
+
+LOCK TABLES `votes` WRITE;
+/*!40000 ALTER TABLE `votes` DISABLE KEYS */;
+INSERT INTO `votes` VALUES (1,2,5),(2,3,6),(3,4,7),(4,2,5),(5,4,7),(6,3,6),(7,3,6),(8,3,6),(9,2,5);
+/*!40000 ALTER TABLE `votes` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -131,4 +189,4 @@ CREATE TABLE `votes` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-03-07  9:27:50
+-- Dump completed on 2017-03-15 20:28:04
